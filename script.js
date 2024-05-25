c689e0d3f890fa9dc439fad966ee295c0719dbd3
Nathan <haloreach252@gmail.com>
2024-05-25 04:42:28 -0600

    Made the site look a lot better, added darkening effect

--- script.js
+++ script.js
@@ -5,6 +5,22 @@ function colorDiv(e) {
         selectedColor = randomizeColor();
     }
 
+    if (shouldDarken === true) {
+        let currentOpacity = e.target.style.opacity;
+
+        if (currentOpacity === "") {
+            currentOpacity = 0;
+        }
+
+        let newOpacity = parseFloat(currentOpacity) + 0.1;
+
+        if (newOpacity > 1) {
+            newOpacity = 1;
+        }
+
+        e.target.style.opacity = newOpacity;
+    }
+
     e.target.style.background = selectedColor;
 }
 
@@ -19,13 +35,13 @@ function randomizeColor() {
     return color
 }
 
-function generateDivs(width, height) {
-    // Validates our width and height, capping at 100x100
-    width = width > 100 ? 100 : width;
-    height = height > 100 ? 100 : height;
+function generateDivs(size) {
+    // Validates our size, ensuring its between 2 and 100
+    size = size > 100 ? 100 : size;
+    size = size < 2 ? 2 : size;
 
-    divWidth = gridDivContainerSize / width;
-    divHeight = gridDivContainerSize / height;
+    divWidth = gridDivContainerSize / size;
+    divHeight = divWidth;
 
     // Clear our current divs
     gridDivContainer.innerHTML = "";
@@ -33,7 +49,7 @@ function generateDivs(width, height) {
     colorDivArray = [];
 
     // Generate our new divs
-    for (let i = 0; i < height; i++) {
+    for (let i = 0; i < size; i++) {
         // Create a new row
         let rowDiv = document.createElement("div");
         rowDiv.className = "gridDivRow";
@@ -43,7 +59,7 @@ function generateDivs(width, height) {
 
         gridDivContainer.appendChild(rowDiv);
 
-        for (let j = 0; j < width; j++) {
+        for (let j = 0; j < size; j++) {
             // Create our new div and apply style
             let createdDiv = document.createElement("div");
             createdDiv.style.background = "#FFFFFF";
@@ -69,9 +85,11 @@ function generateDivs(width, height) {
 
 // Const
 const gridDivContainerSize = 480;
+const initialGridSize = 16;
 
 // General Variables
 let shouldRandomizeColor = false;
+let shouldDarken = false;
 
 // DOM Elements
 let colorDivArray = [];
@@ -79,6 +97,7 @@ let colorDivArray = [];
 const gridDivContainer = document.querySelector("#gridDivContainer");
 
 const randomizeColorCheckbox = document.querySelector("#randomColorCheckbox");
+const shouldDarkenCheckbox = document.querySelector("#shouldDarkenCheckbox");
 const generateDivsButton = document.querySelector("#generateDivsButton");
 const gridSizeInput = document.querySelector("#gridSizeInput");
 
@@ -87,9 +106,13 @@ randomizeColorCheckbox.addEventListener('change', (e) => {
     shouldRandomizeColor = !shouldRandomizeColor;
 });
 
+shouldDarkenCheckbox.addEventListener('change', (e) => {
+    shouldDarken = !shouldDarken;
+});
+
 generateDivsButton.addEventListener('click', (e) => {
     generateDivs(parseInt(gridSizeInput.value), parseInt(gridSizeInput.value));
 });
 
 // Initial Grid
-generateDivs(8, 8);
+generateDivs(initialGridSize, initialGridSize);
